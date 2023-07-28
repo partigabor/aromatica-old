6ea37bcc816caaef02bf28b37714f2b6e9edcddf
Gabor Parti <partigaborhk@gmail.com>
2023-07-28 13:07:22 +0800

    relaunch

---
+++ public/hu/index.search.js
@@ -0,0 +1,23 @@
+var relearn_search_index = [
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Kategóriák",
+    "uri": "/hu/categories/index.html"
+  },
+  {
+    "content": "Magyar verzió\n",
+    "description": "",
+    "tags": null,
+    "title": "Otthon",
+    "uri": "/hu/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Címkék",
+    "uri": "/hu/tags/index.html"
+  }
+]
