602cdb12b49b6219986e607fba947a9c5d6c5209
Gabor Parti <partigaborhk@gmail.com>
2023-08-01 14:51:10 +0800

    disableExplicitIndexURLs = true

--- public/index.search.js
+++ public/index.search.js
@@ -4,48 +4,48 @@ var relearn_search_index = [
     "description": "",
     "tags": null,
     "title": "Spices",
-    "uri": "/spices/index.html"
+    "uri": "/spices/"
   },
   {
     "content": "",
     "description": "",
     "tags": null,
     "title": "Herbs",
-    "uri": "/herbs/index.html"
+    "uri": "/herbs/"
   },
   {
     "content": "",
     "description": "",
     "tags": null,
     "title": "Incense",
-    "uri": "/incense/index.html"
+    "uri": "/incense/"
   },
   {
     "content": "",
     "description": "",
     "tags": null,
     "title": "Perfume",
-    "uri": "/perfume/index.html"
+    "uri": "/perfume/"
   },
   {
     "content": "Welcome to Aromatica, a database containing information about spices, herbs, incense, and other aromatics.\nThe name aromatica is inspired by the words exoticum and its plural exotica. Aromatica is the singular feminine and the plural neuter nominative form of arōmāticus (f. arōmātica, n. arōmāticum), an adjective meaning “composed of spice, aromatic, fragrant” (Lewis \u0026 Short, 1879), cf. Ancient Greek ἀρωματικός arōmatikós adj. ‘aromatic’, and the etymon ἄρωμα árōma n. “aromatic herb or spice” (Liddell–Scott–Jones, 1843).\nIn the past, these meanings included anything fragrant, including oils and perfume, and this website aims to collect the whole spectrum of materials with special olfactory and gustatory qualities.\nThe website is currently under construction, come back in 2024.\n",
     "description": "",
     "tags": null,
     "title": "Home",
-    "uri": "/index.html"
+    "uri": "/"
   },
   {
     "content": "",
     "description": "",
     "tags": null,
     "title": "Categories",
-    "uri": "/categories/index.html"
+    "uri": "/categories/"
   },
   {
     "content": "",
     "description": "",
     "tags": null,
     "title": "Tags",
-    "uri": "/tags/index.html"
+    "uri": "/tags/"
   }
 ]
