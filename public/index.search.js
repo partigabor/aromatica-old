4a6872c1b3666e0860d37ebd9a73750091af4201
Gabor Parti <partigaborhk@gmail.com>
2023-08-21 10:34:06 +0800

    update

--- public/index.search.js
+++ public/index.search.js
@@ -1,24 +1,12 @@
 var relearn_search_index = [
   {
-    "content": "Welcome to Aromatica, a database containing information about spices, herbs, incense, and other aromatics.\nThe name aromatica is inspired by the words exoticum and its plural exotica. Aromatica is the singular feminine and the plural neuter nominative form of arōmāticus (f. arōmātica, n. arōmāticum), an adjective meaning “composed of spice, aromatic, fragrant” (Lewis \u0026 Short, 1879), cf. Ancient Greek ἀρωματικός arōmatikós adj. ‘aromatic’, and the etymon ἄρωμα árōma n. “aromatic herb or spice” (Liddell–Scott–Jones, 1843).\nIn the past, these meanings included anything fragrant, including oils and perfume, and this website aims to collect the whole spectrum of materials with special olfactory and gustatory qualities.\nThe website is currently under construction, come back in 2024.\nVersioning \u0026 Updates v0.0.1. 2023-08-01 Website creation; entering development mode (alpha) ",
-    "description": "",
-    "tags": null,
-    "title": "Home",
-    "uri": "/"
-  },
-  {
-    "content": "The name mountain pepper is an absolute misnomer, since it is not a pepper in any of the botanical senses of the word: it’s not related to chilies, nor the pepper of the Piper family.\n",
-    "description": "",
-    "tags": null,
-    "title": "Mountain pepper",
-    "uri": "/spices/mountain_pepper/"
-  },
-  {
-    "content": "Lorem Ipsum.\n",
-    "description": "",
-    "tags": null,
-    "title": "Spices",
-    "uri": "/spices/"
+    "content": " Illustration of Vanilla planifolia Jacks. ex Andrews from Köhler’s Medizinal-Pflanzen (1887)\nVanilla (Vanilla planifolia Jacks. ex Andrews) is a culinary spice from the Orchidaceae family,[^powo] originating in the region(s) of Tropical America.[^van_wyk_culinary_2014] It is used for its fruit, primarily for flavouring; baking; desserts; perfumery; aromatherapy. Its aroma is described as fragrant, sweet, delicious, with a heat index of 1.[^ucla_medicinal_2002]\nEnglish Arabic Chinese Hungarian vanilla فانيليا 香草 vanília Overview id vanilla species name Vanilla planifolia Jacks. ex Andrews family Orchidaceae part used fruit macroarea Americas region of origin Tropical America cultivation Madagascar; Indonesia; Mexico; Papua New Guinea; China color dark brown pod; creamy white extract botanical database POWO Etymologies English vanilla * \u003c Spanish vainilla ‘id.’ [little sheath, little pod], from \\textit{vaina/vaína} `scabbard, sheath; pod, husk’ + \\textit{-illa} diminutive suffix \u003c Latin vāgīna ‘scabbard, sheath; covering, holder of anything’, esp. husks that enclose an ear of grain; also by anatomical figurative sense, origin of vagina (\rCitation: Oxford University Press, , p. vanilla Oxford University Press \r(n.d.).\r \rOxford English Dictionary.\r \rOxford University Press. Retrieved from \rhttps://www.oed.com/\r; No matching key was found for `[538]{gomez_de_silva_elseviers_1985}[596]{corominas_breve_1987}` in the references. Please make sure to provide an available ID in your `bib.json` file.\rCitation: Lewis \u0026 Short, 1879, p. vāgīna Lewis, \rC. \u0026 Short, \rC. \r(1879).\r \rA Latin Dictionary: Founded on Andrews’ Edition of Freund’s Latin Dictionary, Revised, Enlarged, and in Great Part Rewritten.\r \rClarendon Press. Retrieved from \rhttp://www.perseus.tufts.edu/hopper/search\r)\rArabic فانيليا، فانيلا fānīliyā, fānīllā ‘vanilla’ (no /v/ sound in arabic, rendered with /f/ or /w/) \u003c? French vanilia ‘vanilla’ (No matching key was found for `baalbaki_-mawrid_1995` in the references. Please make sure to provide an available ID in your `bib.json` file.)\rMandarin Chinese 香草 xiāngcǎo ‘vanilla’ [fragrant-grass] (also Cantonese 雲呢拿 \\textit{wan4 nei1 laa4-2}) (\rCitation: ,  (n.d.). Retrieved from \rhttps://www.mdbg.net\r)\r**\nNames English term source vanilla OED Bourbon vanilla Wikipedia Tahitian vanilla Wikipedia French vanilla Wikipedia Arabic script term literal source فانيليا fānīliyā Baalbaki, 1995 ونيلية wanīliyya Baalbaki, 1995 Chinese script term literal source 香草 xiāngcǎo fragrant-grass/herb Defrancis, 2003 ",
+    "description": "The Aromatic Seed Pods Of Orchid Species From America",
+    "tags": [
+      "culinary"
+    ],
+    "title": "Vanilla",
+    "uri": "/materials/vanilla/"
   },
   {
     "content": "",
@@ -31,22 +19,22 @@ var relearn_search_index = [
     "content": "",
     "description": "",
     "tags": null,
-    "title": "Herbs",
-    "uri": "/herbs/"
+    "title": "Tag | culinary",
+    "uri": "/tags/culinary/"
+  },
+  {
+    "content": " Spices\nHerbs\nIncense\nPerfume\n",
+    "description": "",
+    "tags": null,
+    "title": "Materials",
+    "uri": "/materials/"
   },
   {
     "content": "",
     "description": "",
     "tags": null,
-    "title": "Incense",
-    "uri": "/incense/"
-  },
-  {
-    "content": "",
-    "description": "",
-    "tags": null,
-    "title": "Perfume",
-    "uri": "/perfume/"
+    "title": "Category | spice",
+    "uri": "/categories/spice/"
   },
   {
     "content": "",
@@ -54,5 +42,12 @@ var relearn_search_index = [
     "tags": null,
     "title": "Tags",
     "uri": "/tags/"
+  },
+  {
+    "content": "Welcome to Aromatica, a database containing information about spices, herbs, incense, and other aromatics, aiming to gather whole spectrum of these unique materials.\nOn the Name The name aromatica was inspired by the word exotica.1 Aromatica is the singular feminine or the plural neuter nominative form of arōmāticus (f. arōmātica, n. arōmāticum), an adjective meaning “composed of spice, aromatic, fragrant” (Lewis \u0026 Short, 1879), cf. Ancient Greek ἀρωματικός arōmatikós adj. ‘aromatic’, and the etymon ἄρωμα árōma n. “aromatic herb or spice” (Liddell–Scott–Jones, 1843).\nThe term therefore refers to any substance of fragrance, focusing on spices, but also including, oils and perfume, incense, fragrant woods, medicine, and other aromatic exotica with special olfactory and gustatory qualities.\nThe website is currently under construction, come back in 2024.\nVersioning \u0026 Updates v0.0.1. (2023-08-01) website creation: entering development mode (alpha) objects considered interesting because they are out of the ordinary, especially because they originated in a distant foreign country ↩︎\n",
+    "description": "",
+    "tags": null,
+    "title": "Home",
+    "uri": "/"
   }
 ]
